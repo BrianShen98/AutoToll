22a1bda2e973693e8838d2751bc3131f26fdce06
kylewong975 <kyle.wong975@gmail.com>
2017-11-28 21:58:53 -0800

    Add login, register screens

--- app/src/main/java/com/example/csm117/autotoll/MainActivity.java
+++ app/src/main/java/com/example/csm117/autotoll/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.csm117.autotoll;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // register button functionality
+        final Button button_reg = (Button)findViewById(R.id.button_register);
+        button_reg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+        // login button functionality
+        final Button button_log = findViewById(R.id.button_login);
+        button_log.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
+
+
 }
